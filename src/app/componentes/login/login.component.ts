60b6a362cc7db8e5a8ef5649de248374ce092355
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2018-11-07 22:30:26 -0300

    se incorpora recaptcha al login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
@@ -13,15 +13,24 @@ export class LoginComponent implements OnInit {
   email: string;
   password: string;
 
-  signin = new FormGroup({
-    email: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required),
-    captcha: new FormControl(),
+  // signin = new FormGroup({
+  //   email: new FormControl(null, Validators.required),
+  //   password: new FormControl(null, Validators.required),
+  //   captcha: new FormControl(),
+  // });
+
+  public version = VERSION.full;
+  public reactiveForm: FormGroup = new FormGroup({
+      recaptchaReactive: new FormControl(null, Validators.required)
   });
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  resolved(captchaResponse: string) {
+    console.log(`Resolved captcha with response ${captchaResponse}:`);
   }
 
 }
