930c42070ffd80c844e2137327f7bdb302287d36
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2018-11-07 22:34:04 -0300

    se importan modulos para utilizar recaptcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Componentes para formularios (ReactiveFormsModule - recaptcha)
+import { FormsModule, FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
 // Importo componentes para captcha
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgxCaptchaModule } from 'ngx-captcha';
+import { RecaptchaModule, RECAPTCHA_SETTINGS, RecaptchaSettings } from 'ng-recaptcha';
+import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
+import { RECAPTCHA_LANGUAGE } from 'ng-recaptcha';
+
 
 // Componentes a rutear
 import { AppComponent } from './app.component';
@@ -21,9 +26,19 @@ import { RegistroComponent } from './componentes/registro/registro.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    NgxCaptchaModule
+    FormsModule,
+    ReactiveFormsModule,
+    RecaptchaModule.forRoot(),
+    RecaptchaFormsModule,
   ],
-  providers: [],
+  providers: [
+    // {
+    //   provide: RECAPTCHA_SETTINGS,
+    //   useValue: {
+    //     siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+    //   } as RecaptchaSettings,
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
