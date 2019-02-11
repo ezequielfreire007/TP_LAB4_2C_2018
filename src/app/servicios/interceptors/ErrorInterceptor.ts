b3a2fe0685d6be48206f5cad76da95a3f718084c
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-02-10 23:07:51 -0300

    se agrega ErrorInterceptro

---
+++ src/app/servicios/interceptors/ErrorInterceptor.ts
@@ -0,0 +1,23 @@
+// tslint:disable-next-line:eofline
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private _auth: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe( catchError(err => {
+      if (err.status === 401) {
+        this._auth.logoutEmpleado();
+        location.reload();
+      }
+      const error = err.error.message || err.statusText;
+      return throwError(error);
+    }));
+  }
+}
